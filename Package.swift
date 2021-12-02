14e81610bbf0e2d48850206411a6023d7237e514
Paresh Mittal <paresh@edcast.com>
2021-12-02 23:43:43 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ import PackageDescription
 let package = Package(
     name: "RichEditorView",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v11)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -22,6 +22,7 @@ let package = Package(
         .target(
             name: "RichEditorView",
             dependencies: [],
+            path: "Sources",
             resources: [.process("Resources")]
         ),
         .testTarget(
