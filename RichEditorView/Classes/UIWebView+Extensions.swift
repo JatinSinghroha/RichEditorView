f6b97b2f6363055a12072f22a5a59eaa2ef0882d
Jatin Singhroha <my895012@gmail.com>
2023-09-19 20:30:01 +0530

    fixed error

--- RichEditorView/Classes/UIWebView+Extensions.swift
+++ RichEditorView/Classes/UIWebView+Extensions.swift
@@ -68,7 +68,7 @@ extension WKWebView {
         })
 
         while !finished {
-            RunLoop.current.run(mode: RunLoopMode.defaultRunLoopMode, before: .distantFuture)
+            RunLoop.current.run(mode: RunLoop.Mode.default, before: .distantFuture)
         }
     }
 }
